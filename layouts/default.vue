03bbe887cc507a235105a4a0730e7a8114504cd5
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-03-01 18:43:41 +0100

    added header nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,30 @@
 <template>
     
     <div>
-        <nav>
-            <h1>Header</h1>
-        </nav>
+        <header>
+            <img src="/shared/desktop/logo.svg" alt="logo audiophile"/>
+
+            <nav>
+                <NuxtLink to="">Home</NuxtLink>
+                <NuxtLink to="">Headphones</NuxtLink>
+                <NuxtLink to="">Speakers</NuxtLink>
+                <NuxtLink to="">Earphones</NuxtLink>
+                
+            </nav>
+
+
+        </header>
         <slot></slot>
     </div>
 
 </template>
+
+
+<style lang="scss" scoped>
+
+header{
+    background-color: $black;
+    color: $white;
+}
+
+</style>
