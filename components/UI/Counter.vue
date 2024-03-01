52e6248d1deb793c778fa0eebe2a8e88b1c2c43a
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-03-01 18:27:28 +0100

    Clean up

--- components/UI/Counter.vue
+++ components/UI/Counter.vue
@@ -1,39 +1,33 @@
 <template>
-    <div>
-        <button @click="number--">-</button>
-        {{ number }}
-        <button @click="number++">+</button>
-    </div>
+  <div>
+    <button @click="number--">-</button>
+    {{ number }}
+    <button @click="number++">+</button>
+  </div>
 </template>
 
 <script lang="ts" setup>
-    let number = useState('counter-number', ()=>1)
-
+let number = useState("counter-number", () => 1);
 </script>
 
-
 <style lang="scss" scoped>
+div {
+  background-color: $p-grey;
+  padding: 1.5rem 2rem;
+  font-size: 1.5rem;
+  font-weight: bold;
+  display: flex;
+  justify-content: space-between;
+}
 
-    div{
-        background-color: $p-grey;
-        padding: 1.5rem 2rem;
-        font-size: 1.5rem;
-        font-weight: bold;
-        display: flex;
-        justify-content: space-between;
-    }
+button {
+  background-color: rgba(0, 0, 0, 0);
+  border: none;
+  color: $p-dark-grey;
+  font-size: 1.5rem;
 
-    button{
-        background-color: rgba(0,0,0,0);
-        border: none ;
-        color: $p-dark-grey;
-        font-size: 1.5rem;
-    
-        &:hover{
-            color: $primary;
-        }
-    }
-
-
-
+  &:hover {
+    color: $primary;
+  }
+}
 </style>
