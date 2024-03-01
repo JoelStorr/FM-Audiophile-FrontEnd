6ab0c7524bf7e2503e5bb3510bf403b06504ae40
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-03-01 22:03:36 +0100

    Added Cart card for header

---
+++ components/CartCard.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+        <div class="header">
+            <h6>Cart (3)</h6>
+            <a>Remove all</a>
+        </div>
+        <div class="total">
+            <p>Total</p>
+            <h6>$5,396</h6>
+        </div>
+        <UIButtonPrimary>Checkout</UIButtonPrimary>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .container{
+        background-color: $white;
+        width: 25vw;
+        border-radius: 2.5rem;
+        padding:0 2rem 2rem 2rem;
+        display: flex;
+        flex-direction: column;
+    }
+    .header{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    a{
+        font-size: 1.5rem;
+        text-decoration: underline;
+        color: $p-dark-grey;
+    }
+
+
+    .total{
+        text-transform: uppercase;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+    }
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,16 +2,24 @@
     
     <div>
         <header>
+
             <img src="/shared/desktop/logo.svg" alt="logo audiophile"/>
 
             <nav>
-                <NuxtLink to="">Home</NuxtLink>
-                <NuxtLink to="">Headphones</NuxtLink>
-                <NuxtLink to="">Speakers</NuxtLink>
-                <NuxtLink to="">Earphones</NuxtLink>
-                
+                <NuxtLink class="nav-link" to="/">Home</NuxtLink>
+                <NuxtLink class="nav-link" to="/Headphones">Headphones</NuxtLink>
+                <NuxtLink class="nav-link" to="/Speakers">Speakers</NuxtLink>
+                <NuxtLink class="nav-link" to="/Earphones">Earphones</NuxtLink>  
             </nav>
+            
+            <button class="cart-btn" @click="switchCard">
+                <img src="/shared/desktop/icon-cart.svg" alt="Cart"/>
+            </button>
 
+            <div class="cart-card" v-if="showCartCard">
+                <CartCard />
+            </div>
+            <div class="bg-dark" v-if="showCartCard" @click="switchCard"></div>
 
         </header>
         <slot></slot>
@@ -19,12 +27,63 @@
 
 </template>
 
+<script setup>
+    
+
+    let showCartCard = useState('showCartCard', ()=>false);
+
+    function switchCard(){
+        showCartCard.value = !showCartCard.value;
+    }
+
+</script>
+
 
 <style lang="scss" scoped>
 
 header{
+    position: relative;
     background-color: $black;
+    
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 2rem 15%;
+
+}
+
+.nav-link{
+    font-size: 1.3rem;
+    margin: 0 2rem;
+    text-decoration: none;
     color: $white;
+}
+
+.nav-link:visited{
+    color: $white;
+}
+
+.cart-btn{
+    background-color: rgba(0,0,0,0);
+    border: none;
+}
+
+.cart-card{
+    position: absolute;
+    
+    top: 10rem;
+    right: 10%;
+    z-index: 2;
+}
+
+.bg-dark{
+    z-index: 1;
+    position: absolute;
+    top:0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(0,0,0,.4);
 }
 
 </style>
