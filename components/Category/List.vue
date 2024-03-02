59b033026e1b2c149746cc6246262fe4294775a7
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-03-02 23:50:31 +0100

    Reworked Category Card

---
+++ components/Category/List.vue
@@ -0,0 +1,18 @@
+<template>
+    <section class="category-cards">
+    <CategoryCard name="Headphones" to="/headphones" img-src="/shared/desktop/image-category-thumbnail-headphones.png"/>
+     <CategoryCard name="Speakers" to="/headphones" img-src="/shared/desktop/image-category-thumbnail-speakers.png"/>
+      <CategoryCard name="Earphones" to="/headphones" img-src="/shared/desktop/image-category-thumbnail-earphones.png"/>
+   </section>
+
+</template>
+
+<style scoped>
+
+.category-cards{
+    padding: 20rem 10% 15rem 10%;
+    display: flex;
+    justify-content: space-between;
+}
+
+</style>
