72260493e7c72065fddac1de72bea6def5b42f1e
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-03-02 16:20:42 +0100

    Added primary dark button

---
+++ components/UI/Button/PrimaryDark.vue
@@ -0,0 +1,32 @@
+<template>
+
+    <button @click="emit('onClick')" >
+    <slot></slot>
+    </button>
+
+
+</template>
+<script lang="ts" setup>
+    const emit = defineEmits(['onClick'])
+
+
+</script>
+
+<style lang="scss" scoped>
+
+button {
+    background-color: $black;
+    border: none;
+    font-weight: bold;
+    text-transform: uppercase;
+    font-size: 1.3rem;
+    letter-spacing: .1rem;
+    color: $white;
+    padding: 1rem 2rem;
+
+    &:hover{
+        background-color: $secondary;
+    }
+};
+
+</style>
