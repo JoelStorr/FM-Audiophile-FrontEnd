7d373348532a186ef322360ab2be6e8d4a446808
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-03-02 23:52:46 +0100

    Added haedphone category page

--- pages/Headphones/index.vue
+++ pages/Headphones/index.vue
@@ -1,3 +1,29 @@
 <template>
-    <h1>Headphones</h1>
+  <div class="header">
+    <h2>Headphones</h2>
+  </div>
+  <section class="products">
+    <ProductPrev :imgLeft="true" />
+    <ProductPrev :imgLeft="false" />
+    <ProductPrev :imgLeft="true" />
+  </section>
+  <CategoryList />
+  <AudioText />
 </template>
+
+<style lang="scss" scoped>
+.header {
+  padding-top: 5rem;
+  background-color: $black;
+  height: 20vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: $white;
+}
+
+.products {
+  width: 100vw;
+  padding: 0 10%;
+}
+</style>
