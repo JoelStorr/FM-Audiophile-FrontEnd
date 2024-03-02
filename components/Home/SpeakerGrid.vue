5af61cb5589e34ab93f3fea58aae8873b1eec46b
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-03-02 21:24:53 +0100

    Fixed spacing

--- components/Home/SpeakerGrid.vue
+++ components/Home/SpeakerGrid.vue
@@ -1,131 +1,125 @@
 <template>
-
-    <div class="speaker-hero">
-        <div class="img-holder">
-            <img class="speaker" src="/home/desktop/image-speaker-zx9.png" />
-        </div>
-        <img class="rings" src="/home/desktop/pattern-circles.svg" />
-        <div class="content-holder">
-            <h1>ZX9 <br/> Speaker</h1>
-            <p>Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.</p>
-            <div>
-                <UIButtonPrimaryDark>See Product</UIButtonPrimaryDark>
-                <div class="btn-spacer"></div>
-            </div>
-        </div>
+  <div class="speaker-hero">
+    <div class="img-holder">
+      <img class="speaker" src="/home/desktop/image-speaker-zx9.png" />
     </div>
-    <div class="speaker-product">
-        <h2>ZX7 Speaker</h2>
-        <UIButtonSecondary></UIButtonSecondary>
+    <img class="rings" src="/home/desktop/pattern-circles.svg" />
+    <div class="content-holder">
+      <h1>
+        ZX9 <br />
+        Speaker
+      </h1>
+      <p>
+        Upgrade to premium speakers that are phenomenally built to deliver truly
+        remarkable sound.
+      </p>
+      <div>
+        <UIButtonPrimaryDark>See Product</UIButtonPrimaryDark>
+        <div class="btn-spacer"></div>
+      </div>
     </div>
+  </div>
+  <div class="speaker-product">
+    <h2>ZX7 Speaker</h2>
+    <UIButtonSecondary></UIButtonSecondary>
+  </div>
 
-    <div class="headphones-product">
-        <img src="/home/desktop/image-earphones-yx1.jpg" />
-        <div class="">
-            <h3>YX1 Earphones</h3>
-            <UIButtonSecondary></UIButtonSecondary>
-        </div>
+  <div class="headphones-product">
+    <img src="/home/desktop/image-earphones-yx1.jpg" />
+    <div class="">
+      <h3>YX1 Earphones</h3>
+      <UIButtonSecondary></UIButtonSecondary>
     </div>
-
+  </div>
 </template>
 
-<script setup>
-
-
-</script>
-
+<script setup></script>
 
 <style lang="scss" scoped>
+.speaker-hero {
+  position: relative;
+  width: 100%;
+  height: 50rem;
+  background-color: $primary;
+  display: flex;
+  overflow: hidden;
+  border-radius: 1rem;
+  margin-bottom: 5rem;
+}
 
+.img-holder {
+  position: relative;
+  height: 100%;
 
-.speaker-hero{
-    position: relative;
-    width: 100%;
-    height: 50rem;
-    background-color: $primary;
-    display: flex;
-    overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+}
+
+.speaker {
+  position: relative;
+  z-index: 1;
+  width: 60%;
+  height: auto;
+  top: 10px;
+}
+
+.rings {
+  position: absolute;
+  width: 90%;
+  top: -10%;
+  left: -20%;
+}
+
+.content-holder {
+  width: 60%;
+  color: $white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  z-index: 1;
+  padding-right: 10rem;
+
+  & h1 {
+    margin: 0;
+  }
+
+  & p {
+    margin: 4rem 0;
+  }
+}
+
+.speaker-product {
+  width: 100%;
+  padding: 7rem;
+  background-image: url("/home/desktop/image-speaker-zx7.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  border-radius: 1rem;
+}
+
+.headphones-product {
+  margin-top: 5rem;
+  display: flex;
+
+  & img {
     border-radius: 1rem;
-    margin-bottom: 5rem;
-}
+    margin-right: 1rem;
 
-.img-holder{
-    position: relative;
-    height: 100%;
-    
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: flex-end;
-    
-}
+    flex: 1;
+  }
 
-.speaker{
-    position: relative;
-    z-index: 1;
-    width: 60%;
-    height: auto;
-    top: 10px;
-}
-
-.rings{
-    position: absolute;
-    width: 90%;
-    top: -10%;
-    left: -20%;
-}
-
-.content-holder{
-    width: 60%;
-    color: $white;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    z-index: 1;
-    padding-right: 10rem;
-
-
-    & h1{
-        margin: 0;
-    }
-
-    & p{
-        margin: 4rem 0;
-    }
-}
-
-.speaker-product{
-    width: 100%;
+  & div {
+    flex: 2;
+    background-color: $p-grey;
+    margin-left: 1rem;
+    border-radius: 1rem;
     padding: 5rem;
-    background-image: url("/home/desktop/image-speaker-zx7.jpg");
-    background-size: cover;
-    background-repeat: no-repeat;
-    border-radius: 1rem;
-}
-
-.headphones-product{
-    margin-top: 5rem;
     display: flex;
-
-    & img{
-        border-radius: 1rem;
-        margin-right: 1rem;
-        
-        flex: 1;
-    }
-
-
-    & div{
-        flex: 2;
-        background-color: $p-grey;
-        margin-left: 1rem;
-        border-radius: 1rem;
-        padding: 5rem;
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        justify-content: center;
-    }
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+  }
 }
-
 </style>
