761b482368bbf3ddc4a3123ae0b20584e04f7814
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-03-03 16:06:10 +0100

    Added related products

---
+++ components/RelatedProducts/Element.vue
@@ -0,0 +1,35 @@
+<template>
+
+    <div class="content-holder">
+
+       
+            <img class="product-image" src="/product-xx99-mark-one-headphones/desktop/image-category-page-preview.jpg" />
+      
+        <h3>XX99 Mark |</h3>
+        <UIButtonPrimary>See Product</UIButtonPrimary>
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+
+
+    .content-holder{
+        position: relative;
+        width: 100%;
+        margin: 2rem;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+
+    .product-image{
+        width: 100%;
+        height: auto;
+        border-radius: 1rem;
+    }
+
+
+</style>
---
+++ components/RelatedProducts/List.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="related-product-lits">
+        <RelatedProductsElement />
+        <RelatedProductsElement />
+        <RelatedProductsElement />
+
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+    .related-product-lits{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+        width: 100%;
+        position: relative;
+    }
+
+</style>
