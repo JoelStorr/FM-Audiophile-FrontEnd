214cfda6a3d92103f60f6ca82d4392cdc59c9835
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-03-03 16:05:04 +0100

    Added base layout

--- pages/Earphones/index.vue
+++ pages/Earphones/index.vue
@@ -1,3 +1,27 @@
 <template>
-    <h1>Earphones</h1>
+  <div class="header">
+    <h2>Earphones</h2>
+  </div>
+  <section class="products">
+    <ProductPrev :imgLeft="true" />
+  </section>
+  <CategoryList />
+  <AudioText />
 </template>
+
+<style lang="scss" scoped>
+.header {
+  padding-top: 5rem;
+  background-color: $black;
+  height: 20vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: $white;
+}
+
+.products {
+  width: 100vw;
+  padding: 0 10%;
+}
+</style>
