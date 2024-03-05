8654fe19c97b70e938aa46c8d991f4f33e66f2e1
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-03-05 19:26:10 +0100

    Reworked middleware

--- middleware/setup.global.js
+++ middleware/setup.global.js
@@ -5,11 +5,9 @@ import { useMainStore } from "~/store/index";
 export default defineNuxtRouteMiddleware( async(to, from) => {
     const store = useMainStore();
     const { loadProducst, getProduct } = store;
-
+    
     if(store.products.length == 0){
          await loadProducst();
     }
-
-    
 
 });
