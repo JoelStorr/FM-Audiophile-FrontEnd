3f8ecbfadef66e00235c718098b4b29834f131d2
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-03-09 09:31:11 +0100

    changes for publish

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,9 +6,7 @@ export default defineNuxtConfig({
     "~/assets/style/main.scss", // you should add main.scss somewhere in your app
   ],
 
-  modules:[
-    '@pinia/nuxt',
-  ],
+  modules: ["@pinia/nuxt"],
 
   vite: {
     css: {
@@ -16,6 +14,21 @@ export default defineNuxtConfig({
         scss: {
           additionalData: '@use "~/assets/style/_colors.scss" as *;',
         },
+      },
+    },
+  },
+
+  ssr: false,
+  app: {
+    baseURL: "/JSFMAudiophile/", // baseURL: '/<repository>/'
+    buildAssetsDir: "assets", // don't use "_" at the begining of the folder name to avoids nojkill conflict
+
+    head: {
+      charset: "utf-8",
+      title: "devlinks",
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }],
+      htmlAttrs: {
+        lang: "en",
       },
     },
   },
